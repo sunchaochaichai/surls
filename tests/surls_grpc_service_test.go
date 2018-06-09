ad337e3fd495fbcb87ebe0b47b1bdd70ab31a334
xiaolei.guo <xiaolei.guo@sinobbd.com>
2018-06-09 18:33:28 +0800

    update

--- tests/surls_grpc_service_test.go
+++ tests/surls_grpc_service_test.go
@@ -7,6 +7,68 @@ import (
 	"reflect"
 )
 
+func TestSurlsSet(t *testing.T) {
+	tests := []struct {
+		in   string
+		want pb.SetResp
+	}{
+		{
+			in: "http://a.com",
+			want: pb.SetResp{
+				Code: 200,
+				Msg:  "success",
+				Data: &pb.SetRespData{
+					SourceUrl: "http://a.com",
+					ShortUrl:  "388495550d157e5c5d299b7ecfeb1c2d",
+				},
+			},
+		},
+		{
+			in: "http://b.com",
+			want: pb.SetResp{
+				Code: 200,
+				Msg:  "success",
+				Data: &pb.SetRespData{
+					SourceUrl: "http://b.com",
+					ShortUrl:  "715af3fd14408eda374ef3bcb23c10b6",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		req := &pb.SetReq{Url: tt.in}
+		resp, err := surlsGrpcService.Set(context.Background(), req)
+
+		if err != nil {
+			t.Error("surls set test failed", err)
+		}
+
+		r := pb.SetResp{
+			Code: resp.Code,
+			Msg:  resp.Msg,
+			Data: resp.Data,
+		}
+
+		if !reflect.DeepEqual(r.Data, tt.want.Data) {
+			t.Errorf(
+				"surls get(%s) = %s , wanted %s",
+				tt.in,
+				resp.String(),
+				tt.want.String(),
+			)
+		}
+	}
+}
+
+func BenchmarkSurlsSet(b *testing.B) {
+	b.ResetTimer()
+	req := &pb.SetReq{Url: "http://www.baidu.com"}
+	for i := 0; i < b.N; i++ {
+		surlsGrpcService.Set(context.Background(), req)
+	}
+}
+
 func TestSurlsGet(t *testing.T) {
 
 	tests := []struct {
@@ -14,7 +76,7 @@ func TestSurlsGet(t *testing.T) {
 		want pb.GetResp
 	}{
 		{
-			in: "http://a.com",
+			in: "388495550d157e5c5d299b7ecfeb1c2d",
 			want: pb.GetResp{
 				Code: 200,
 				Msg:  "success",
@@ -22,11 +84,11 @@ func TestSurlsGet(t *testing.T) {
 			},
 		},
 		{
-			in: "http://b.com/c/d?e=f",
+			in: "715af3fd14408eda374ef3bcb23c10b6",
 			want: pb.GetResp{
 				Code: 200,
 				Msg:  "success",
-				Data: "http://b.com/c/d?e=f",
+				Data: "http://b.com",
 			},
 		},
 	}
@@ -45,7 +107,7 @@ func TestSurlsGet(t *testing.T) {
 			Data: resp.Data,
 		}
 
-		if !reflect.DeepEqual(r, tt.want) {
+		if !reflect.DeepEqual(r.Data, tt.want.Data) {
 			t.Errorf(
 				"surls get(%s) = %s , wanted %s",
 				tt.in,
@@ -61,70 +123,5 @@ func BenchmarkSurlsGet(b *testing.B) {
 	req := &pb.GetReq{Url: "http://www.baidu.com"}
 	for i := 0; i < b.N; i++ {
 		surlsGrpcService.Get(context.Background(), req)
-	}
-}
-
-func TestSurlsSet(t *testing.T) {
-	tests := []struct {
-		in   string
-		want pb.SetResp
-	}{
-		{
-			in: "http://a.com",
-			want: pb.SetResp{
-				Code: 200,
-				Msg:  "success",
-				Data: &pb.SetRespData{
-					SourceUrl: "http://a.com",
-					ShortUrl:  "http://a.com",
-				},
-			},
-		},
-		{
-			in: "http://b.com",
-			want: pb.SetResp{
-				Code: 200,
-				Msg:  "success",
-				Data: &pb.SetRespData{
-					SourceUrl: "http://b.com",
-					ShortUrl:  "http://b.com",
-				},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		req := &pb.SetReq{Url: tt.in}
-		resp, err := surlsGrpcService.Set(context.Background(), req)
-
-		if err != nil {
-			t.Error("surls set test failed", err)
-		}
-
-		r := pb.SetResp{
-			Code: resp.Code,
-			Msg:  resp.Msg,
-			Data: &pb.SetRespData{
-				SourceUrl: tt.in,
-				ShortUrl:  tt.in,
-			},
-		}
-
-		if !reflect.DeepEqual(r, tt.want) {
-			t.Errorf(
-				"surls set(%s) = %s , wanted %s",
-				tt.in,
-				resp.String(),
-				tt.want.String(),
-			)
-		}
-	}
-}
-
-func BenchmarkSurlsSet(b *testing.B) {
-	b.ResetTimer()
-	req := &pb.SetReq{Url: "http://www.baidu.com"}
-	for i := 0; i < b.N; i++ {
-		surlsGrpcService.Set(context.Background(), req)
 	}
 }
